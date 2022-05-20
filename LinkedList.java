f2d5a368979a66669cc36d57450e1c7d700fcbb0
YUON-ElvisLui <97438126+YUON-ElvisLui@users.noreply.github.com>
2022-05-20 16:09:29 +0800

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,61 +1,113 @@
 public class LinkedList {
 
-    public static void main(String []args) {
-        LinkedList hi = new LinkedList();
-        hi.addToHead("KP");
-        hi.addToHead("KP");
-        hi.addToHead("KP");
-        hi.printList();
-
-
+  public static void main(String []args) {
+    LinkedList testList = new LinkedList();
+    for (int i = 0; i <= 10; i++) {
+      testList.addToTail(i);
     }
 
-    public simpleNode head;
+    testList.printList();
+    swapNodes(testList, 3, 7);
+    testList.printList();
+  }  
 
-    public LinkedList() {
-        this.head = null;
+  public Node head;
+
+  public static void swapNodes(LinkedList list, int data1, int data2) {
+    System.out.println("Swapping " + data1 + " with " + data2);
+  
+    Node node1Prev = null;
+    Node node2Prev = null;
+    Node node1 = list.head;
+    Node node2 = list.head;
+
+    if (data1 == data2) {
+      System.out.println("Elements are the same - no swap to be made");
+      return;
+    }
+  
+    while (node1 != null) {
+      if (node1.data == data1) { 
+        break;
+      }
+      node1Prev = node1;
+      node1 = node1.getNextNode();
+    }
+  
+    while (node2 != null) {
+      if (node2.data == data2) {
+        break;
+      }
+      node2Prev = node2;
+      node2 = node2.getNextNode();
+    }
+  
+    if (node1 == null || node2 == null) {
+      System.out.println("Swap not possible - one or more element is not in the list");
+      return;
     }
 
-    public void addToHead(String data) {
-        simpleNode newHead = new simpleNode(data);
-        simpleNode currentHead = this.head;
-        this.head = newHead;
-        if (currentHead != null) {
-            this.head.setNextNode(currentHead);
-        }
+    if (node1Prev == null) {
+      list.head = node2;
+    } else {
+      node1Prev.setNextNode(node2);
     }
 
-    public void addToTail(String data) {
-        simpleNode tail = this.head;
-        if (tail == null) {
-            this.head = new simpleNode(data);
-        } else {
-            while (tail.getNextNode() != null) {
-                tail = tail.getNextNode();
-            }
-            tail.setNextNode(new simpleNode(data));
-        }
+    if (node2Prev == null) { 
+      list.head = node1;
+    } else {
+      node2Prev.setNextNode(node1);
     }
+  
+    Node temp = node1.getNextNode();
+    node1.setNextNode(node2.getNextNode());
+    node2.setNextNode(temp); 
+  }
 
-    public String removeHead() {
-        simpleNode removedHead = this.head;
-        if (removedHead == null) {
-            return null;
-        }
-        this.head = removedHead.getNextNode();
-        return removedHead.data;
-    }
+  public LinkedList() {
+    this.head = null;
+  }
 
-    public String printList() {
-        String output = "<head> ";
-        simpleNode currentsimpleNode = this.head;
-        while (currentsimpleNode != null) {
-            output += currentsimpleNode.data + " ";
-            currentsimpleNode = currentsimpleNode.getNextNode();
-        }
-        output += "<tail>";
-        System.out.println(output);
-        return output;
+  public void addToHead(int data) {
+    Node newHead = new Node(data);
+    Node currentHead = this.head;
+    this.head = newHead;
+    if (currentHead != null) {
+      this.head.setNextNode(currentHead);
     }
+  }
+
+  public void addToTail(int data) {
+    Node tail = this.head;
+    if (tail == null) {
+      this.head = new Node(data);
+    } else {
+      while (tail.getNextNode() != null) {
+        tail = tail.getNextNode();
+      }
+        tail.setNextNode(new Node(data));
+    }
+  }  
+
+  public Integer removeHead() {
+    Node removedHead = this.head;
+    if (removedHead == null) {
+      return null;
+    }
+    this.head = removedHead.getNextNode();
+    return removedHead.data;
+  } 
+
+  public String printList() {
+    String output = "<head> ";
+    Node currentNode = this.head;
+    while (currentNode != null) {
+      output += currentNode.data + " ";
+      currentNode = currentNode.getNextNode();
+    }
+    output += "<tail>";
+    System.out.println(output);
+    return output;
+  }
 
 }
